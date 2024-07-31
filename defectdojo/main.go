a6caa7ea8ea113d614dd98768845b54107372940
tharun13055 <megatharun13055@gmail.com>
2024-07-31 22:30:44 +0530

    add: Defectdojo is config fix: It will create the engagement and push the file

--- defectdojo/main.go
+++ defectdojo/main.go
@@ -5,21 +5,16 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/blackaichi/go-defectdojo/defectdojo"
 )
-
-type ScanResult struct {
-	FilePath    string `json:"file_path"`
-	Severity    string `json:"severity"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-}
 
 func main() {
 	url := "http://13.201.28.37:8080"
 	authorizedToken := "798b581a65f991dc728d2ae8745a77f6459078bc"
 
+	// Create DefectDojo client
 	dj, err := defectdojo.NewDojoClient(url, authorizedToken, nil)
 	if err != nil {
 		log.Fatalf("Failed to create DefectDojo client: %v", err)
@@ -44,35 +39,51 @@ func main() {
 			if engagement.Name != nil {
 				name = *engagement.Name
 			}
-
 			fmt.Printf("Engagement ID: %d, Name: %s\n", id, name)
 		}
 	} else {
 		fmt.Println("No engagements found")
 	}
 
-	filePath := "/home/tharun/axonaio/sample/paynpro/result/trivy_output.json"
-	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Printf("The error is %v\n", err)
+		return
+	}
+
+	// Define the relative file path
+	filePath := "result/trivy_output.json"
+
+	// Join the current working directory with the file path
+	fullPath := filepath.Join(cwd, filePath)
+
+	// Check if the file exists
+	if _, err := os.Stat(fullPath); os.IsNotExist(err) {
 		log.Fatalf("File does not exist: %v", err)
 	}
 
 	// Create ImportScan struct
 	importScan := &defectdojo.ImportScan{
-		MinimumSeverity: ptrString("info"),
-		Active:          ptrBool(true),
-		Verified:        ptrBool(true),
-		File:            ptrString(filePath),
+		MinimumSeverity:   ptrString("Info"),
+		Active:            ptrBool(true),
+		Verified:          ptrBool(true),
+		File:              ptrString(filePath),
+		EngagementId:      ptrInt(17),
+		ScanType:          ptrString("Trivy Scan"),
+		Environment:       ptrString("Development"),
+		AutoCreateContext: ptrBool(true),
+		ProductName:       ptrString("myproject"),
+		ProductTypeName:   ptrString("Research and Development"),
 	}
+
+	// Debugging: print importScan details
+	fmt.Printf("ImportScan details: %+v\n", importScan)
 
 	// Use the Create method to import scan results
 	importedScan, err := dj.ImportScan.Create(cxt, importScan)
 	if err != nil {
 		// Print error details
 		fmt.Printf("Failed to import scan results: %v\n", err)
-		if httpErr, ok := err.(defectdojo.HTTPError); ok {
-			fmt.Printf("HTTP Status Code: %d\n", httpErr.StatusCode)
-			fmt.Printf("Response Body: %s\n", httpErr.Body)
-		}
 	} else {
 		fmt.Printf("Scan results imported successfully: %+v\n", importedScan)
 	}
@@ -85,4 +96,8 @@ func ptrString(s string) *string {
 
 func ptrBool(b bool) *bool {
 	return &b
+}
+
+func ptrInt(i int) *int {
+	return &i
 }
